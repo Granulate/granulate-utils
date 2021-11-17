a0fb3313a5a930cc3b4fe037060f5a9234309c35
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-11-17 02:03:47 +0200

    Add linters etc from gProfiler @ f1c59bc4f4079d

---
+++ lint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [ -f venv/bin/activate ]; then
+  source venv/bin/activate
+fi
+
+check_arg=""
+if [[ "$1" = "--ci" ]]; then
+    check_arg="--check"
+fi
+
+isort --settings-file .isort.cfg .
+black --line-length 120 $check_arg .
+flake8 --config .flake8 .
+mypy .
