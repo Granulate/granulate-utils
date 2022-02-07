660bd65b6c56e7c1338a0f652ef6d595911e9d21
David Dorfman <david.dorfman@granulate.io>
2022-02-07 16:07:55 +0200

    linux: Add methods to acquire and release system-wide mutex

--- granulate_utils/exceptions.py
+++ granulate_utils/exceptions.py
@@ -2,3 +2,8 @@ class UnsupportedNamespaceError(Exception):
     def __init__(self, nstype: str):
         super().__init__(f"Namespace {nstype!r} is not supported by this kernel")
         self.nstype = nstype
+
+
+class CouldNotAcquireMutex(Exception):
+    def __init__(self, name) -> None:
+        super().__init__(f'Could not acquire mutex {name!r}. Another process might be holding it.')
