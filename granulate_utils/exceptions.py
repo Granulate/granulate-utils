786313ba3a8314d09da1472b4af78cb2065c4d83
David Dorfman <david.dorfman@granulate.io>
2022-02-13 20:45:38 +0200

    add missing license header to exceptions.py

--- granulate_utils/exceptions.py
+++ granulate_utils/exceptions.py
@@ -1,3 +1,8 @@
+#
+# Copyright (c) Granulate. All rights reserved.
+# Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
+#
+
 class UnsupportedNamespaceError(Exception):
     def __init__(self, nstype: str):
         super().__init__(f"Namespace {nstype!r} is not supported by this kernel")
