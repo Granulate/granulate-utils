e0cab1a59e3b050360886eac45e668fc96df2907
Michel Heily <michel.heily@granulate.io>
2022-03-06 11:02:41 +0200

    linux: ns: Add full set of NsType

--- granulate_utils/linux/ns.py
+++ granulate_utils/linux/ns.py
@@ -29,10 +29,13 @@ _SENTINEL = _Sentinel()
 
 
 class NsType(enum.IntFlag):
-    mnt = 0x00020000  # CLONE_NEWNS
-    net = 0x40000000  # CLONE_NEWNET
-    pid = 0x20000000  # CLONE_NEWPID
-    uts = 0x04000000  # CLONE_NEWUTS
+    mnt = 0x00020000     # CLONE_NEWNS
+    net = 0x40000000     # CLONE_NEWNET
+    pid = 0x20000000     # CLONE_NEWPID
+    uts = 0x04000000     # CLONE_NEWUTS
+    cgroup = 0x02000000  # CLONE_NEWCGROUP
+    ipc = 0x08000000     # CLONE_NEWIPC
+    user = 0x10000000    # CLONE_NEWUSER
 
 
 libc: Optional[ctypes.CDLL] = None
