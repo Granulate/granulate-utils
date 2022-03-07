7146bbbbd043a301173b084bd16f45b00b08c41f
Tomer Yogev <tomer.yogev@granulate.io>
2022-03-07 18:29:17 +0200

    Revert change to linter

--- lint.sh
+++ lint.sh
@@ -13,7 +13,7 @@ fi
 # see also isort --skip and flake8 config.
 EXCLUDE_RE='venv/bin/|build|granulate_utils/generated'
 
-isort --settings-path .isort.cfg --skip granulate_utils/generated -rc .
+isort --settings-path .isort.cfg --skip granulate_utils/generated .
 black --line-length 120 $check_arg --exclude $EXCLUDE_RE .
 flake8 --config .flake8  .
 mypy --exclude $EXCLUDE_RE .
