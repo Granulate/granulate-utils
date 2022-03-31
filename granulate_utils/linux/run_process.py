376893bbc541dffc39e3c852f4ed7c8d6b4496b9
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-03-31 14:17:11 +0300

    Transform run_process to a class
    This makes it more managable and will allow further refactoring in the future (for the better).
    Currently required for gProfiler to allow overriding some of the behavior.

--- granulate_utils/linux/run_process.py
+++ granulate_utils/linux/run_process.py
@@ -7,23 +7,13 @@ import os
 import signal
 import subprocess
 import time
-from functools import partial
 from threading import Event
 from typing import Any, Callable, List, Optional, Tuple, Union
 
 from granulate_utils.exceptions import CalledProcessError, CalledProcessTimeoutError, ProcessStoppedException
 from granulate_utils.linux.process import prctl
 
-PR_SET_PDEATHSIG = 1
-
 LoggerType = Union[logging.Logger, logging.LoggerAdapter]
-
-
-def set_child_termination_on_parent_death(logger: LoggerType) -> None:
-    try:
-        prctl(PR_SET_PDEATHSIG, signal.SIGTERM)
-    except OSError as e:
-        logger.warning(f"Failed to set parent-death signal on child process. errno: {e.errno}, strerror: {e.strerror}")
 
 
 def _wrap_callbacks(callbacks: List[Callable]) -> Callable:
@@ -39,39 +29,129 @@ def _wrap_callbacks(callbacks: List[Callable]) -> Callable:
     return wrapper
 
 
-def start_process(
-    cmd: List[str], logger: LoggerType, term_on_parent_death: bool = True, **kwargs: Any
-) -> subprocess.Popen:
-    logger.debug(f"Running command: ({' '.join(cmd)})")
+class RunProcess:
+    def __init__(
+        self,
+        cmd: List[str],
+        logger: LoggerType,
+        stop_event: Event = None,
+        suppress_log: bool = False,
+        check: bool = True,
+        timeout: int = None,
+        kill_signal: signal.Signals = signal.SIGKILL,
+        communicate: bool = True,
+        stdin: bytes = None,
+    ) -> None:
+        self.cmd = cmd
+        self.logger = logger
+        self.stop_event = stop_event
+        self.suppress_log = suppress_log
+        self.check = check
+        self.timeout = timeout
+        self.kill_signal = kill_signal
+        self.communicate = communicate
+        self.stdin = stdin
 
-    cur_preexec_fn = kwargs.pop("preexec_fn", os.setpgrp)
-    if term_on_parent_death:
-        cur_preexec_fn = _wrap_callbacks([partial(set_child_termination_on_parent_death, logger), cur_preexec_fn])
+        self.process: Optional[subprocess.Popen] = None
 
-    popen = subprocess.Popen(
-        cmd,
-        stdout=kwargs.pop("stdout", subprocess.PIPE),
-        stderr=kwargs.pop("stderr", subprocess.PIPE),
-        stdin=subprocess.PIPE,
-        preexec_fn=cur_preexec_fn,
-        **kwargs,
-    )
-    return popen
+    def start(self, term_on_parent_death: bool = True, **popen_kwargs: Any) -> subprocess.Popen:
+        assert self.process is None, "process already started!"
+        self.logger.debug(f"Running command: ({' '.join(self.cmd)})")
 
+        cur_preexec_fn = popen_kwargs.pop("preexec_fn", os.setpgrp)
+        if term_on_parent_death:
+            cur_preexec_fn = _wrap_callbacks([self._set_child_termination_on_parent_death, cur_preexec_fn])
 
-def _reap_process(
-    process: subprocess.Popen,
-    kill_signal: signal.Signals,
-    logger: LoggerType,
-) -> Tuple[int, str, str]:
-    # kill the process and read its output so far
-    process.send_signal(kill_signal)
-    process.wait()
-    logger.debug(f"({process.args!r}) was killed by us with signal {kill_signal} due to timeout or stop request")
-    stdout, stderr = process.communicate()
-    returncode = process.poll()
-    assert returncode is not None  # only None if child has not terminated
-    return returncode, stdout, stderr
+        self.process = subprocess.Popen(
+            self.cmd,
+            stdout=popen_kwargs.pop("stdout", subprocess.PIPE),
+            stderr=popen_kwargs.pop("stderr", subprocess.PIPE),
+            stdin=subprocess.PIPE,
+            preexec_fn=cur_preexec_fn,
+            **popen_kwargs,
+        )
+        return self.process
+
+    def _set_child_termination_on_parent_death(self, logger: LoggerType) -> None:
+        PR_SET_PDEATHSIG = 1
+        try:
+            prctl(PR_SET_PDEATHSIG, signal.SIGTERM)
+        except OSError as e:
+            self.logger.warning(
+                f"Failed to set parent-death signal on child process. errno: {e.errno}, strerror: {e.strerror}"
+            )
+
+    def _reap_process(
+        self,
+    ) -> Tuple[int, str, str]:
+        assert self.process is not None, "process not started!"
+        # kill the process and read its output so far
+        self.process.send_signal(self.kill_signal)
+        self.process.wait()
+        self.logger.debug(
+            f"({self.process.args!r}) was killed by us with signal {self.kill_signal} due to timeout or stop request"
+        )
+        stdout, stderr = self.process.communicate()
+        returncode = self.process.poll()
+        assert returncode is not None  # only None if child has not terminated
+        return returncode, stdout, stderr
+
+    def run(self) -> "subprocess.CompletedProcess[bytes]":
+        stdout = None
+        stderr = None
+        reraise_exc: Optional[BaseException] = None
+        with self.start():
+            assert self.process is not None, "process was not started?"
+            try:
+                communicate_kwargs = dict(input=self.stdin) if self.stdin is not None else {}
+                if self.stop_event is None:
+                    assert self.timeout is None, f"expected no timeout, got {self.timeout!r}"
+                    if self.communicate:
+                        # wait for stderr & stdout to be closed
+                        stdout, stderr = self.process.communicate(timeout=self.timeout, **communicate_kwargs)
+                    else:
+                        # just wait for the process to exit
+                        self.process.wait()
+                else:
+                    end_time = (time.monotonic() + self.timeout) if self.timeout is not None else None
+                    while True:
+                        try:
+                            if self.communicate:
+                                stdout, stderr = self.process.communicate(timeout=1, **communicate_kwargs)
+                            else:
+                                self.process.wait(timeout=1)
+                            break
+                        except subprocess.TimeoutExpired:
+                            if self.stop_event.is_set():
+                                raise ProcessStoppedException from None
+                            if end_time is not None and time.monotonic() > end_time:
+                                assert self.timeout is not None
+                                raise
+            except subprocess.TimeoutExpired:
+                returncode, stdout, stderr = self._reap_process()
+                assert self.timeout is not None
+                reraise_exc = CalledProcessTimeoutError(self.timeout, returncode, self.cmd, stdout, stderr)
+            except BaseException as e:  # noqa
+                returncode, stdout, stderr = self._reap_process()
+                reraise_exc = e
+            retcode = self.process.poll()
+            assert retcode is not None  # only None if child has not terminated
+
+        result: subprocess.CompletedProcess[bytes] = subprocess.CompletedProcess(
+            self.process.args, retcode, stdout, stderr
+        )
+
+        self.logger.debug(f"({self.process.args!r}) exit code: {result.returncode}")
+        if not self.suppress_log:
+            if result.stdout:
+                self.logger.debug(f"({self.process.args!r}) stdout: {result.stdout.decode()!r}")
+            if result.stderr:
+                self.logger.debug(f"({self.process.args!r}) stderr: {result.stderr.decode()!r}")
+        if reraise_exc is not None:
+            raise reraise_exc
+        elif self.check and retcode != 0:
+            raise CalledProcessError(retcode, self.process.args, output=stdout, stderr=stderr)
+        return result
 
 
 def run_process(
@@ -84,57 +164,5 @@ def run_process(
     kill_signal: signal.Signals = signal.SIGKILL,
     communicate: bool = True,
     stdin: bytes = None,
-    **kwargs: Any,
 ) -> "subprocess.CompletedProcess[bytes]":
-    stdout = None
-    stderr = None
-    reraise_exc: Optional[BaseException] = None
-    with start_process(cmd, logger, **kwargs) as process:
-        try:
-            communicate_kwargs = dict(input=stdin) if stdin is not None else {}
-            if stop_event is None:
-                assert timeout is None, f"expected no timeout, got {timeout!r}"
-                if communicate:
-                    # wait for stderr & stdout to be closed
-                    stdout, stderr = process.communicate(timeout=timeout, **communicate_kwargs)
-                else:
-                    # just wait for the process to exit
-                    process.wait()
-            else:
-                end_time = (time.monotonic() + timeout) if timeout is not None else None
-                while True:
-                    try:
-                        if communicate:
-                            stdout, stderr = process.communicate(timeout=1, **communicate_kwargs)
-                        else:
-                            process.wait(timeout=1)
-                        break
-                    except subprocess.TimeoutExpired:
-                        if stop_event.is_set():
-                            raise ProcessStoppedException from None
-                        if end_time is not None and time.monotonic() > end_time:
-                            assert timeout is not None
-                            raise
-        except subprocess.TimeoutExpired:
-            returncode, stdout, stderr = _reap_process(process, kill_signal, logger)
-            assert timeout is not None
-            reraise_exc = CalledProcessTimeoutError(timeout, returncode, cmd, stdout, stderr)
-        except BaseException as e:  # noqa
-            returncode, stdout, stderr = _reap_process(process, kill_signal, logger)
-            reraise_exc = e
-        retcode = process.poll()
-        assert retcode is not None  # only None if child has not terminated
-
-    result: subprocess.CompletedProcess[bytes] = subprocess.CompletedProcess(process.args, retcode, stdout, stderr)
-
-    logger.debug(f"({process.args!r}) exit code: {result.returncode}")
-    if not suppress_log:
-        if result.stdout:
-            logger.debug(f"({process.args!r}) stdout: {result.stdout.decode()!r}")
-        if result.stderr:
-            logger.debug(f"({process.args!r}) stderr: {result.stderr.decode()!r}")
-    if reraise_exc is not None:
-        raise reraise_exc
-    elif check and retcode != 0:
-        raise CalledProcessError(retcode, process.args, output=stdout, stderr=stderr)
-    return result
+    return RunProcess(cmd, logger, stop_event, suppress_log, check, timeout, kill_signal, communicate, stdin).run()
