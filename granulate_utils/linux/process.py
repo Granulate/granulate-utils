f70b33a46cfc295e3357f1ff5fbed2b3fc1863d2
David Dorfman <david.dorfman@granulate.io>
2022-07-26 18:46:01 +0300

    Move process utils from elf.py to process.py

--- granulate_utils/linux/process.py
+++ granulate_utils/linux/process.py
@@ -3,9 +3,18 @@
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
 
+import struct
 import psutil
+from typing import Optional
 
 from granulate_utils.exceptions import MissingExePath
+from granulate_utils.linux.elf import get_elf_id
+
+
+_AUXV_ENTRY = struct.Struct("LL")
+
+AT_EXECFN = 31
+PATH_MAX = 4096
 
 
 def process_exe(process: psutil.Process) -> str:
@@ -39,3 +48,50 @@ def is_musl(process: psutil.Process) -> bool:
     # TODO: make sure no glibc libc.so file exists (i.e, return True if musl, False if glibc, and raise
     # if not conclusive)
     return any("ld-musl" in m.path for m in process.memory_maps())
+
+
+def get_mapped_dso_elf_id(process: psutil.Process, dso_part: str) -> Optional[str]:
+    """
+    Searches for a DSO path containing "dso_part" and gets its elfid.
+    Returns None if not found.
+    """
+    for m in process.memory_maps():
+        if dso_part in m.path:
+            # don't need resolve_proc_root_links here - paths in /proc/pid/maps are normalized.
+            return get_elf_id(f"/proc/{process.pid}/root/{m.path}")
+    else:
+        return None
+
+
+def _read_process_auxv(process: psutil.Process, auxv_id: int) -> int:
+    try:
+        with open(f"/proc/{process.pid}/auxv", "rb") as f:
+            auxv = f.read()
+    except FileNotFoundError:
+        raise psutil.NoSuchProcess(process.pid)
+
+    for i in range(0, len(auxv), _AUXV_ENTRY.size):
+        entry = auxv[i : i + _AUXV_ENTRY.size]
+        id_, val = _AUXV_ENTRY.unpack(entry)
+
+        if id_ == auxv_id:
+            assert isinstance(val, int)  # mypy fails to understand
+            return val
+    else:
+        raise ValueError(f"auxv id {auxv_id} was not found!")
+
+
+def _read_process_memory(process: psutil.Process, addr: int, size: int) -> bytes:
+    try:
+        with open(f"/proc/{process.pid}/mem", "rb", buffering=0) as mem:
+            mem.seek(addr)
+            return mem.read(size)
+    except FileNotFoundError:
+        raise psutil.NoSuchProcess(process.pid)
+
+
+def read_process_execfn(process: psutil.Process) -> str:
+    # reads process AT_EXECFN
+    addr = _read_process_auxv(process, AT_EXECFN)
+    fn = _read_process_memory(process, addr, PATH_MAX)
+    return fn[: fn.index(b"\0")].decode()
