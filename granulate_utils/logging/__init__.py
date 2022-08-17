ef1d6726d5c020bf3d73345837a87be0b1407c9f
David Dorfman <david.dorfman@granulate.io>
2022-08-18 01:46:49 +0300

    Limit messages buffer to size in MB

--- granulate_utils/logging/__init__.py
+++ granulate_utils/logging/__init__.py
@@ -2,13 +2,14 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
+import json
 import logging
 import threading
 import time
 import uuid
 from datetime import datetime
 from logging import Handler, LogRecord
-from typing import Any, Dict, List, Tuple, Union
+from typing import List, NamedTuple, Tuple, Union
 
 import backoff
 import requests
@@ -19,6 +20,61 @@ REQUEST_MAX_TRIES = 5
 logger = logging.getLogger(__name__)
 
 
+class Batch(NamedTuple):
+    """A collection of messages sent to the server."""
+
+    logs: List[bytes]
+    size: int
+    head_serial_no: int
+
+
+class MessagesBuffer:
+    """
+    A list of strings limited by the total length of all items.
+    Keeps count of current number of items, and dropped and added items.
+    Not thread-safe on its own!
+    """
+
+    def __init__(self, max_total_length: int, overflow_drop_factor: float):
+        self.max_total_length = max_total_length
+        self.overflow_drop_factor = overflow_drop_factor
+        self.total_length = 0
+        self.buffer: List[bytes] = []
+        self.lengths: List[int] = []
+        self.head_serial_no = 0
+        self.next_serial_no = 0
+
+    @property
+    def count(self) -> int:
+        return len(self.buffer)
+
+    @property
+    def utilization(self) -> float:
+        return self.total_length / self.max_total_length
+
+    def append(self, item):
+        self.buffer.append(item)
+        self.lengths.append(len(item))
+        self.total_length += len(item)
+        self.next_serial_no += 1
+        self.check_overflow()
+
+    def check_overflow(self) -> None:
+        if self.total_length >= self.max_total_length:
+            self.drop(int(self.overflow_drop_factor * self.count))
+
+    def drop(self, n: int):
+        assert n > 0, "n must be positive!"
+        if self.count == 0:
+            return
+        if n > self.count:
+            n = self.count
+        self.head_serial_no += n
+        self.total_length -= sum(self.lengths[:n])
+        del self.lengths[:n]
+        del self.buffer[:n]
+
+
 class BatchRequestsHandler(Handler):
     """
     logging.Handler that accumulates logs in a buffer and flushes them periodically or when
@@ -27,106 +83,133 @@ class BatchRequestsHandler(Handler):
     """
 
     scheme = "https"
-    capacity = 100 * 1000  # max number of records to buffer
-    flush_interval = 10.0  # maximum amount of seconds between flushes
-    flush_threshold = 0.8  # force flush if buffer size reaches this percentage of capacity
-    overflow_drop_factor = 1 - flush_threshold  # drop this percentage of capacity upon overflow
 
     # If Tuple[float, float], then the first value is connection-timeout and the second read-timeout.
     # See https://docs.python-requests.org/en/latest/api/#requests.request
     request_timeout: Union[float, Tuple[float, float]] = 1.5
 
-    def __init__(self, server_address: str):
+    def __init__(
+        self,
+        server_address: str,
+        max_message_size=1 * 1024 * 1024,  # 1mb
+        max_total_length=5 * 1024 * 1024,  # 5mb
+        flush_interval=10.0,
+        flush_threshold=0.8,
+        overflow_drop_factor=0.25,
+    ):
         super().__init__(logging.DEBUG)
-        self.head_serial_no: int = -1
-        self.next_serial_no: int = 0
-        self.buffer: List[dict] = []
-        self.server_address = server_address
-        self.session = requests.Session()
+        self.max_message_size = max_message_size  # maximum message size
+        self.capacity = max_total_length  # maximum size of buffer in bytes
+        self.flush_interval = flush_interval  # maximum amount of seconds between flushes
+        self.flush_threshold = flush_threshold  # force flush if buffer size reaches this percentage of capacity
+        self.overflow_drop_factor = overflow_drop_factor  # drop this percentage of messages upon overflow
+        self.messages_buffer = MessagesBuffer(self.capacity, self.overflow_drop_factor)
         self.stop_event = threading.Event()
         self.time_fn = time.time
+        self.server_address = server_address
+        self.session = requests.Session()
         self.last_flush_time = 0.0
         self.flush_thread = threading.Thread(target=self._flush_loop, daemon=True, name="Logs flusher")
-        self.setup()
-        self.flush_thread.start()
+        self.start()
 
-    def setup(self) -> None:
-        """Called to perform setup before the flush thread is started."""
-        pass
+    def start(self) -> None:
+        """Called to start the flush thread."""
+        self.flush_thread.start()
 
     def emit(self, record: LogRecord) -> None:
         # Called while lock is acquired
-        item = self.dictify_record(record)
-        self.buffer.append(item)
-        self.next_serial_no += 1
-        self.check_overflow()
+        item = self.dump_record_locked(record)
+        self.messages_buffer.append(item)
 
-    def check_overflow(self) -> None:
-        if len(self.buffer) >= self.capacity:
-            drop_count = int(self.overflow_drop_factor * self.capacity)
-            del self.buffer[:drop_count]
-
-    def dictify_record(self, record: LogRecord) -> Dict[str, Any]:
+    def dump_record_locked(self, record: LogRecord) -> bytes:
         # `format` is required to fill in some record members such as `message` and `asctime`.
         self.format(record)
+        record_dict = self.dictify_record(record)
+        if record.truncated:  # type: ignore
+            record_dict["truncated"] = True
+        record_dict["serial_no"] = self.messages_buffer.next_serial_no
+        return json.dumps(record_dict).encode("utf-8")
+
+    def format(self, record: LogRecord) -> str:
+        """Override to customize record fields."""
+        s = super().format(record)
+        if len(record.message) > self.max_message_size:
+            record.message = record.message[: self.max_message_size]
+            record.truncated = True
+        else:
+            record.truncated = False
+        return s
+
+    def dictify_record(self, record):
         formatted_timestamp = datetime.utcfromtimestamp(record.created).isoformat()
         return {
-            "serial_no": self.next_serial_no,
-            "message": record.message,
             "level": record.levelname,
             "timestamp": formatted_timestamp,
             "logger_name": record.name,
+            "message": record.message,
         }
 
     def _flush_loop(self) -> None:
         self.last_flush_time = self.time_fn()
         while not self.stop_event.is_set():
             if self.should_flush():
-                self._flush()
+                self.flush()
             self.stop_event.wait(1.0)
 
-    def should_flush(self) -> bool:
-        delta_time = self.time_fn() - self.last_flush_time
-        return (len(self.buffer) >= self.flush_threshold * self.capacity) or (delta_time >= self.flush_interval)
+    @property
+    def time_since_last_flush(self) -> float:
+        return self.time_fn() - self.last_flush_time
 
-    def _flush(self) -> None:
+    def should_flush(self) -> bool:
+        return (
+            self.messages_buffer.count > 0
+            and (self.messages_buffer.utilization >= self.flush_threshold)
+            or (self.time_since_last_flush >= self.flush_interval)
+        )
+
+    def flush(self) -> None:
         self.last_flush_time = self.time_fn()
         try:
-            response = self._send_logs()
+            batch, response = self._send_logs()
             response.raise_for_status()
-            last_ingested_serial_no = response.json()["last_ingested_serial_no"]
         except Exception:
-            logger.error("Error posting to server")
+            logger.error(f"Error posting {len(batch.logs)} messages ({batch.size/1024}kb)")
         else:
-            self.remove_up_to_serial_no(last_ingested_serial_no + 1)
+            self.drop_sent_batch(batch)
+
+    def drop_sent_batch(self, sent_batch: Batch) -> None:
+        # we don't override createLock(), so lock is not None
+        with self.lock:  # type: ignore
+            dropped = self.messages_buffer.head_serial_no - sent_batch.head_serial_no
+            n = len(sent_batch.logs) - dropped
+            if n > 0:
+                self.messages_buffer.drop(n)
 
     @backoff.on_exception(backoff.expo, exception=RequestException, max_tries=REQUEST_MAX_TRIES)
-    def _send_logs(self) -> requests.Response:
+    def _send_logs(self) -> Tuple[Batch, requests.Response]:
+        """
+        :return: (batch that was sent, response)
+        """
         # Upon every retry we will remake the batch, in case we are able to batch more messages together.
+        batch = self.make_batch()
         data = {
             "id": uuid.uuid4().hex,
             "metadata": self.get_metadata(),
-            "logs": self.make_batch(),
+            "logs": batch.logs,
         }
-        return self.session.post(f"{self.scheme}://{self.server_address}/", json=data, timeout=self.request_timeout)
+        response = self.session.post(f"{self.scheme}://{self.server_address}/", json=data, timeout=self.request_timeout)
+        return batch, response
 
     def get_metadata(self) -> dict:
         """Called to get metadata per batch."""
         return {}
 
-    def make_batch(self) -> list:
-        self.acquire()
-        batch = self.buffer[:]
-        self.release()
-        return batch
-
-    def remove_up_to_serial_no(self, serial_no: int) -> None:
-        self.acquire()
-        n = serial_no - self.head_serial_no
-        if n > 0:
-            del self.buffer[:n]
-            self.head_serial_no = serial_no
-        self.release()
+    def make_batch(self) -> Batch:
+        # we don't override createLock(), so lock is not None
+        with self.lock:  # type: ignore
+            return Batch(
+                self.messages_buffer.buffer[:], self.messages_buffer.total_length, self.messages_buffer.head_serial_no
+            )
 
     def stop(self) -> bool:
         """
