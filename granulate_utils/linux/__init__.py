5af741284efe5928826469fa887ccaa826183373
Temitope Oluwafemi <temitope.e.oluwafemi@intel.com>
2022-08-18 11:58:06 -0400

    Added changes to support Windows build of gprofiler

--- granulate_utils/linux/__init__.py
+++ granulate_utils/linux/__init__.py
@@ -3,12 +3,23 @@
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
 import os
+import platform
 from typing import Tuple
 
 
 def get_kernel_release() -> Tuple[int, int]:
     """Return Linux kernel version as (major, minor) tuple."""
-    major_str, minor_str = os.uname().release.split(".", maxsplit=2)[:2]
+    major_str = ""
+    minor_str = ""
+    if platform.system() == 'Linux':
+        major_str, minor_str = os.uname().release.split(".", maxsplit=2)[:2]
+    else:
+        release = platform.uname().release
+        if "." in release:
+            major_str, minor_str = platform.uname().release.split(".", maxsplit=2)[:2]
+        else:
+            major_str = release
+            minor_str = "0"
     return int(major_str), int(minor_str)
 
 
