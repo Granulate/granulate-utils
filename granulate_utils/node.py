a3fde1be2fda90b24f6dfdbda49c483cdc8257e3
Ariel Moshe <ariel.moshe@granulate.io>
2022-09-15 15:08:16 +0300

    Changed node process detection

--- granulate_utils/node.py
+++ granulate_utils/node.py
@@ -3,4 +3,12 @@
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
 
-DETECTED_NODE_PROCESSES_REGEX = r"^.+/libnode\.so"
+import os
+
+from psutil import Process
+
+from granulate_utils.linux.process import process_exe
+
+
+def is_node_process(process: Process) -> bool:
+    return os.path.basename(process_exe(process)) == "node"
