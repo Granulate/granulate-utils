e5d26b17c03933cc5dd635391131e8ed80eeaad3
Eyal Halpern Shalev <eyal.shalev@granulate.io>
2022-09-28 15:01:12 +0300

    Add custom UnsupportedCGroupV2 error when initializing BaseCgroup

--- lint.sh
+++ lint.sh
@@ -3,6 +3,8 @@ set -e
 
 if [ -f venv/bin/activate ]; then
   source venv/bin/activate
+elif [ -f .venv/bin/activate ]; then
+  source .venv/bin/activate
 fi
 
 check_arg=""
@@ -11,9 +13,9 @@ if [[ "$1" = "--ci" ]]; then
 fi
 
 # see also isort --skip and flake8 config.
-EXCLUDE_RE='venv|build|granulate_utils/generated'
+EXCLUDE_RE='.venv|venv|build|granulate_utils/generated'
 
-isort --settings-path .isort.cfg --skip granulate_utils/generated --skip venv --skip build .
+isort --settings-path .isort.cfg --skip granulate_utils/generated --skip venv --skip .venv --skip build .
 black --line-length 120 $check_arg --exclude $EXCLUDE_RE .
 flake8 --config .flake8  --exclude $(echo $EXCLUDE_RE | tr '|' ',') .
 mypy --exclude $EXCLUDE_RE .
