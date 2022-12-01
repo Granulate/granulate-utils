fb98185e7270d954c57ac9b0945f3786f43dd7db
Amir Klein <amir.klein@granulate.io>
2022-12-01 17:36:54 +0200

    Added type hinting that was missed by linter

--- tests/granulate_utils/test_cgroups.py
+++ tests/granulate_utils/test_cgroups.py
@@ -4,15 +4,15 @@ from unittest.mock import patch
 from granulate_utils.linux.cgroups.cpu_cgroup import CpuCgroup
 
 
-def wrap_read_from_control_file(mapping: Dict[str, str]):
-    def func(tunable_name: str):
+def wrap_read_from_control_file(mapping: Dict[str, str]) -> Callable:
+    def func(tunable_name: str) -> str:
         return mapping[tunable_name]
 
     return func
 
 
 @patch("granulate_utils.linux.cgroups.cpu_cgroup.BaseCgroup.__init__", return_value=None)
-def test_get_cpu_limit_cores(base_group_mock: Callable):
+def test_get_cpu_limit_cores(base_group_mock: Callable) -> None:
     cpucgroup = CpuCgroup()
     with patch.object(
         cpucgroup,
