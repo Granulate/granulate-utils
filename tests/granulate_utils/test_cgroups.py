d822ec33231ae90d137a61d980d2844c8830f9a1
Amir Klein <amir.klein@granulate.io>
2022-12-05 16:36:54 +0200

    CR Fixes

--- tests/granulate_utils/test_cgroups.py
+++ tests/granulate_utils/test_cgroups.py
@@ -1,39 +1,28 @@
-from typing import Callable, Dict
+from typing import Callable
 from unittest.mock import patch
 
 from granulate_utils.linux.cgroups.cpu_cgroup import CpuCgroup
 
 
-def wrap_read_from_control_file(mapping: Dict[str, str]) -> Callable:
-    def func(tunable_name: str) -> str:
-        return mapping[tunable_name]
-
-    return func
-
-
-@patch("granulate_utils.linux.cgroups.cpu_cgroup.BaseCgroup.__init__", return_value=None)
+@patch("granulate_utils.linux.cgroups.cpu_cgroup.BaseCgroup._verify_preconditions", return_value=None)
 def test_get_cpu_limit_cores(base_group_mock: Callable) -> None:
     cpucgroup = CpuCgroup()
     with patch.object(
         cpucgroup,
         "read_from_control_file",
-        wrap_read_from_control_file(
-            {
-                CpuCgroup.cfs_period_us: "100",
-                CpuCgroup.cfs_quota_us: "50",
-            }
-        ),
+        {
+            CpuCgroup.cfs_period_us: "100",
+            CpuCgroup.cfs_quota_us: "50",
+        }.__getitem__,
     ):
         assert cpucgroup.get_cpu_limit_cores() == 0.5
 
     with patch.object(
         cpucgroup,
         "read_from_control_file",
-        wrap_read_from_control_file(
-            {
-                CpuCgroup.cfs_period_us: "100",
-                CpuCgroup.cfs_quota_us: "-1",
-            }
-        ),
+        {
+            CpuCgroup.cfs_period_us: "100",
+            CpuCgroup.cfs_quota_us: "-1",
+        }.__getitem__,
     ):
         assert cpucgroup.get_cpu_limit_cores() == -1.0
