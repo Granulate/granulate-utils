df5dcde8c41d934d6e304b1583efb99b6ff9ec86
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-12-11 13:39:29 +0200

    glogger: Fix Python 3.8 compatibility

--- glogger/extra_adapter.py
+++ glogger/extra_adapter.py
@@ -21,7 +21,7 @@ class ExtraAdapter(logging.LoggerAdapter):
         non-standard kwargs available under an "extra" key. Default implementation merges existing attributes with
         default ones provided at initialization.
         """
-        return self.extra | kwargs.get("extra", {})
+        return {**self.extra, **kwargs.get("extra", {})}
 
     def process(self, msg: Any, kwargs: MutableMapping[str, Any]) -> Tuple[Any, MutableMapping[str, Any]]:
         # Partition the kwargs into logging kwargs and extra kwargs:
@@ -34,11 +34,11 @@ class ExtraAdapter(logging.LoggerAdapter):
                 other_kwargs[k] = v
 
         # Merge other kwargs into extra:
-        extra = logging_kwargs.get("extra", {}) | other_kwargs
+        extra = {**logging_kwargs.get("extra", {}), **other_kwargs}
         if extra:
             logging_kwargs["extra"] = extra
 
         extra = self.get_extra(**logging_kwargs)
         # Retain all extras as attributes on the record, and add "extra" attribute that contains all the extras:
-        logging_kwargs.update({"extra": extra | {"extra": extra}})
+        logging_kwargs.update({"extra": {**extra, **{"extra": extra}}})
         return msg, logging_kwargs
