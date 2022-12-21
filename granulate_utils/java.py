25b7e40446e7c6d578e904f405621e44f908f25b
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-12-21 13:15:25 +0200

    java: JVM version parsing tests (#112)
    * CI tests for granulate-utils separately
    * Make JvmVersion a dataclass
    * Add JVM parsing tests
    * Remove glogger lint - it's included in granulate-utils lint.

--- granulate_utils/java.py
+++ granulate_utils/java.py
@@ -6,6 +6,7 @@
 import os
 import re
 import signal
+from dataclasses import dataclass
 from itertools import dropwhile
 from typing import Iterable, List, Optional, Union
 
@@ -113,14 +114,11 @@ def java_exit_code_to_signo(exit_code: int) -> Optional[int]:
         return None
 
 
+@dataclass
 class JvmVersion:
-    def __init__(self, version: Version, build: int, name: str):
-        self.version = version
-        self.build = build
-        self.name = name
-
-    def __repr__(self) -> str:
-        return f"JvmVersion({self.version}, {self.build!r}, {self.name!r})"
+    version: Version
+    build: int
+    name: str
 
 
 # Parse java version information from "java -version" output
---
+++ tests/granulate_utils/test_jvm_version.py
@@ -0,0 +1,104 @@
+from typing import Union
+
+import pytest
+from packaging.version import Version
+
+from granulate_utils.java import JvmVersion, parse_jvm_version
+
+
+@pytest.mark.parametrize(
+    "java_version,jvm_version_or_err",
+    [
+        # 8
+        (
+            """openjdk version "1.8.0_352"
+OpenJDK Runtime Environment (build 1.8.0_352-8u352-ga-1~22.04-b08)
+OpenJDK 64-Bit Server VM (build 25.352-b08, mixed mode)
+""",
+            JvmVersion(Version("8.352"), 8, "OpenJDK 64-Bit Server VM"),
+        ),
+        # -ojdkbuild suffix
+        (
+            """openjdk version "1.8.0_201-ojdkbuild"
+OpenJDK Runtime Environment (build 1.8.0_201-ojdkbuild-09)
+OpenJDK 64-Bit Server VM (build 25.201-b09, mixed mode)""",
+            JvmVersion(Version("8.201"), 9, "OpenJDK 64-Bit Server VM"),
+        ),
+        # -internal suffix
+        (
+            """openjdk version "1.8.0_111-internal"
+OpenJDK Runtime Environment (build 1.8.0_111-internal-alpine-r0-b14)
+OpenJDK 64-Bit Server VM (build 25.111-b14, mixed mode)""",
+            JvmVersion(Version("8.111"), 14, "OpenJDK 64-Bit Server VM"),
+        ),
+        # -ea suffix
+        (
+            """openjdk version "13-ea" 2019-09-17
+OpenJDK Runtime Environment (build 13-ea+32)
+OpenJDK 64-Bit Server VM (build 13-ea+32, mixed mode, sharing)""",
+            JvmVersion(Version("13"), 32, "OpenJDK 64-Bit Server VM"),
+        ),
+        # -b appears twice
+        (
+            """openjdk version "1.8.0_342"
+OpenJDK Runtime Environment (build 1.8.0_342-8u342-b07-0ubuntu1~18.04-b07)
+OpenJDK 64-Bit Server VM (build 25.342-b07, mixed mode)""",
+            JvmVersion(Version("8.342"), 7, "OpenJDK 64-Bit Server VM"),
+        ),
+        # 17
+        (
+            """openjdk version "17.0.1" 2021-10-19
+OpenJDK Runtime Environment (build 17.0.1+12-39)
+OpenJDK 64-Bit Server VM (build 17.0.1+12-39, mixed mode, sharing)""",
+            JvmVersion(Version("17.0.1"), 12, "OpenJDK 64-Bit Server VM"),
+        ),
+        # 11
+        (
+            """openjdk version "11.0.12" 2021-07-20
+OpenJDK Runtime Environment (build 11.0.12+7-post-Debian-2deb10u1)
+OpenJDK 64-Bit Server VM (build 11.0.12+7-post-Debian-2deb10u1, mixed mode)""",
+            JvmVersion(Version("11.0.12"), 7, "OpenJDK 64-Bit Server VM"),
+        ),
+        # 7
+        (
+            """java version "1.7.0_251"
+OpenJDK Runtime Environment (amzn-2.6.21.0.82.amzn1-x86_64 u251-b02)
+OpenJDK 64-Bit Server VM (build 24.251-b02, mixed mode)""",
+            JvmVersion(Version("7.251"), 2, "OpenJDK 64-Bit Server VM"),
+        ),
+        # zing 21
+        (
+            """java version "1.8.0_312"
+Java Runtime Environment (Zing 21.12.0.0-b2-linux64) (build 1.8.0_312-b2)
+Zing 64-Bit Tiered VM (Zing 21.12.0.0-b2-linux64) (build 1.8.0_312-zing_21.12.0.0-b3-product-linux-X86_64, mixed mode)""",  # noqa
+            JvmVersion(Version("8.312"), 2, "Zing 64-Bit Tiered VM (Zing 21.12.0.0-b2-linux64)"),
+        ),
+        # zing 22 & java 15
+        (
+            """java version "15.0.8" 2022-08-04
+Java Runtime Environment Zing22.07.1.0+1 (build 15.0.8+4-MTS)
+Zing 64-Bit Tiered VM Zing22.07.1.0+1 (build 15.0.8-zing_22.07.1.0-b1-product-linux-X86_64, mixed mode)""",
+            JvmVersion(Version("15.0.8"), 4, "Zing 64-Bit Tiered VM Zing22.07.1.0+1"),
+        ),
+        # open j9
+        (
+            """openjdk version "1.8.0_332"
+IBM Semeru Runtime Open Edition (build 1.8.0_332-b09)
+Eclipse OpenJ9 VM (build openj9-0.32.0, JRE 1.8.0 Linux amd64-64-Bit Compressed References 20220422_370 (JIT enabled, AOT enabled)"""  # noqa
+            """OpenJ9   - 9a84ec34e
+OMR      - ab24b6666
+JCL      - 0b8b8af39a based on jdk8u332-b09)
+""",
+            JvmVersion(Version("8.332"), 9, "Eclipse OpenJ9 VM"),
+        ),
+        # TODO: add error cases here
+    ],
+)
+def test_parse_jvm_version(java_version: str, jvm_version_or_err: Union[JvmVersion, str]) -> None:
+    if isinstance(jvm_version_or_err, JvmVersion):
+        assert parse_jvm_version(java_version) == jvm_version_or_err
+    else:
+        assert isinstance(jvm_version_or_err, str)
+        with pytest.raises(Exception) as e:
+            parse_jvm_version(java_version)
+        assert e.value.args[0] == jvm_version_or_err
