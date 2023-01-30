e98a0b9eb21f76dab4247ceb958855a80cd043b2
uri ariel <uri.ariel@granulate.io>
2023-01-30 20:01:29 +0200

    Add jvm flags parsing

--- granulate_utils/java.py
+++ granulate_utils/java.py
@@ -8,7 +8,7 @@ import re
 import signal
 from dataclasses import dataclass
 from itertools import dropwhile
-from typing import Iterable, List, Literal, Optional, Union
+from typing import Iterable, List, Literal, Match, Optional, Union
 
 from packaging.version import Version
 
@@ -215,3 +215,85 @@ def parse_jvm_version(version_string: str) -> JvmVersion:
         zing_major = None
 
     return JvmVersion(version, build, vm_name, vm_type, zing_major)
+
+
+@dataclass
+class JvmFlag:
+    name: str
+    type: str
+    value: str
+    origin: str
+    kind: List[str]
+
+    @classmethod
+    def from_match(cls, match: Match) -> "JvmFlag":
+        # get the flag origin if jvm 9+, otherwise get is the flag from non default origin as described above
+        flag_origin_jdk_9 = match.group("flag_origin_jdk_9")
+
+        flag_equal_sign_prefix = match.group("flag_equal_sign_prefix")
+        flag_is_non_default_origin_only_jdk_8 = flag_equal_sign_prefix == ":"
+
+        is_jdk_8 = flag_is_non_default_origin_only_jdk_8 or flag_origin_jdk_9 is None
+
+        # split the list of space separated flag_kinds as described above
+        flag_kind = match.group("flag_kind").split()
+
+        if is_jdk_8:
+            if flag_is_non_default_origin_only_jdk_8:
+                flag_origin = "non-default"
+            else:
+                flag_origin = "default"
+        else:
+            flag_origin = flag_origin_jdk_9
+
+        return cls(
+            name=match.group("flag_name"),
+            type=match.group("flag_type"),
+            value=match.group("flag_value"),
+            origin=flag_origin,
+            kind=sorted(flag_kind),
+        )
+
+
+def parse_jvm_flags(jvm_flags_string: str) -> List[JvmFlag]:
+    """
+    The output of VM.flags -all format on jdk 8:
+    bool UseCompressedClassPointers               := true                                {lp64_product}
+    flag_type flag_name                           := flag_value                          {flag_kind}
+    ":=" indicates non default origin for the flag, while "=" indicates default origin
+
+    The output of VM.flags -all format on jdk 9+:
+    bool OptoScheduling                           = false                               {C2 pd product} {default}
+    flag_type flag_name                           = flag_value                          {flag_kind} {flag_origin}
+
+    flag_kind is space separated list of kinds, e.g. "C2 pd product"
+
+    possible flag kinds: "product", "manageable", "diagnostic", "experimental", "notproduct", "develop",
+    "lp64_product", "rw", "pd", "JVMCI", "C1", "C2", "ARCH"
+
+    possible flag types: "bool", "int", "uint", "intx", "uintx", "uint64_t", "size_t", "double", "ccstr",
+    "ccstrlist"
+
+    possible flag origins: default, non-default, command line, environment, config file, management, ergonomic,
+    attach, internal, jimage, "command line, ergonomic" (flag is set from command line and aligned by ergonomic)
+
+    KNOWN ISSUES:
+    - we don't parse the flag value for ccstrlist type flags (e.g. -XX:CompileCommand='A' -XX:CompileCommand='B')
+    """
+
+    vm_flags = []
+    vm_flags_pattern = re.compile(
+        r"(?P<flag_type>\S+)\s+"
+        r"(?P<flag_name>\S+)\s+"
+        r"(?P<flag_equal_sign_prefix>:)?= "
+        r"(?P<flag_value>\S+)\s*"
+        r"{(?P<flag_kind>.+?)}"
+        r"(?:\s*{(?P<flag_origin_jdk_9>default|non-default|command line|environment|config file|management|ergonomic|attach|internal|jimage|command line, ergonomic)})?"  # noqa: E501
+    )
+
+    for line in jvm_flags_string.splitlines():
+        match = vm_flags_pattern.search(line)
+        if match:
+            vm_flags.append(JvmFlag.from_match(match))
+
+    return vm_flags
---
+++ tests/granulate_utils/test_jvm_flags.py
@@ -0,0 +1,105 @@
+from typing import List
+
+import pytest
+
+from granulate_utils.java import JvmFlag, parse_jvm_flags
+
+
+@pytest.mark.parametrize(
+    "jvm_flags_string,expected_jvm_flags_list",
+    [
+        (
+            """[Global flags]
+    uintx NonNMethodCodeHeapSize                   = 7594288                                {pd product} {ergonomic}
+    uintx NonProfiledCodeHeapSize                  = 122031976                              {pd product} {ergonomic}
+     intx NumberOfLoopInstrToAlign                 = 4                                      {C2 product} {management}
+     intx ObjectAlignmentInBytes                   = 8                                    {lp64_product} {internal}
+   size_t OldPLABSize                              = 1024                                 {ARCH product} {default}
+    uintx OldPLABWeight                            = 50                                        {product} {environment}
+   size_t OldSize                                  = 5452592                                   {product} {default}
+     bool OmitStackTraceInFastThrow                = true                                      {product} {attach}
+ccstrlist OnError                                  = cat hs_err_pid%p.log
+          OnError                                 += ps -ef                                    {product} {command line}
+ccstrlist OnOutOfMemoryError                       =                                     {C2 pd product} {default}
+     intx OnStackReplacePercentage                 = 140                                    {pd product} {config file}\n"""  # noqa: E501
+            """     bool OptimizeFill                             = true                                   {C2 product} {command line, ergonomic}\n"""  # noqa: E501
+            """     bool OptimizePtrCompare                       = true                                   {C2 product} {default}""",  # noqa: E501
+            [
+                JvmFlag(
+                    name="NonNMethodCodeHeapSize",
+                    type="uintx",
+                    value="7594288",
+                    origin="ergonomic",
+                    kind=["pd", "product"],
+                ),
+                JvmFlag(
+                    name="NonProfiledCodeHeapSize",
+                    type="uintx",
+                    value="122031976",
+                    origin="ergonomic",
+                    kind=["pd", "product"],
+                ),
+                JvmFlag(
+                    name="NumberOfLoopInstrToAlign", type="intx", value="4", origin="management", kind=["C2", "product"]
+                ),
+                JvmFlag(
+                    name="ObjectAlignmentInBytes", type="intx", value="8", origin="internal", kind=["lp64_product"]
+                ),
+                JvmFlag(name="OldPLABSize", type="size_t", value="1024", origin="default", kind=["ARCH", "product"]),
+                JvmFlag(name="OldPLABWeight", type="uintx", value="50", origin="environment", kind=["product"]),
+                JvmFlag(name="OldSize", type="size_t", value="5452592", origin="default", kind=["product"]),
+                JvmFlag(name="OmitStackTraceInFastThrow", type="bool", value="true", origin="attach", kind=["product"]),
+                JvmFlag(
+                    name="OnStackReplacePercentage",
+                    type="intx",
+                    value="140",
+                    origin="config file",
+                    kind=["pd", "product"],
+                ),
+                JvmFlag(
+                    name="OptimizeFill",
+                    type="bool",
+                    value="true",
+                    origin="command line, ergonomic",
+                    kind=["C2", "product"],
+                ),
+                JvmFlag(name="OptimizePtrCompare", type="bool", value="true", origin="default", kind=["C2", "product"]),
+            ],
+        ),
+        (
+            """
+    uintx NewSize                                  := 357564416                           {product}
+    uintx NewSizeThreadIncrease                     = 5320                                {pd product}
+     intx NmethodSweepActivity                      = 10                                  {product}
+    uintx OldPLABWeight                             = 50                                  {lp64_product}
+    uintx OldSize                                  := 716177408                           {product}
+     bool OmitStackTraceInFastThrow                 = true                                {product}
+ccstrlist OnError                                  := cat hs_err_pid%p.log
+      OnError                             += ps -ef                              {product}
+ccstrlist OnOutOfMemoryError                        =                                     {product}
+     intx OnStackReplacePercentage                  = 140                                 {pd product}
+     bool OptoBundling                              = false                               {C2 pd product}
+""",
+            [
+                JvmFlag(name="NewSize", type="uintx", value="357564416", origin="non-default", kind=["product"]),
+                JvmFlag(
+                    name="NewSizeThreadIncrease", type="uintx", value="5320", origin="default", kind=["pd", "product"]
+                ),
+                JvmFlag(name="NmethodSweepActivity", type="intx", value="10", origin="default", kind=["product"]),
+                JvmFlag(name="OldPLABWeight", type="uintx", value="50", origin="default", kind=["lp64_product"]),
+                JvmFlag(name="OldSize", type="uintx", value="716177408", origin="non-default", kind=["product"]),
+                JvmFlag(
+                    name="OmitStackTraceInFastThrow", type="bool", value="true", origin="default", kind=["product"]
+                ),
+                JvmFlag(
+                    name="OnStackReplacePercentage", type="intx", value="140", origin="default", kind=["pd", "product"]
+                ),
+                JvmFlag(
+                    name="OptoBundling", type="bool", value="false", origin="default", kind=["C2", "pd", "product"]
+                ),
+            ],
+        ),
+    ],
+)
+def test_parse_jvm_flags(jvm_flags_string: str, expected_jvm_flags_list: List[JvmFlag]) -> None:
+    assert parse_jvm_flags(jvm_flags_string) == expected_jvm_flags_list
