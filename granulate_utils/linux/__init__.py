69c9d1e2b897768786eacbc0426892c1e0a4170e
pfilipko1 <piotr.filipkowski@intel.com>
2023-02-23 13:28:56 +0100

    library change

--- granulate_utils/linux/__init__.py
+++ granulate_utils/linux/__init__.py
@@ -2,13 +2,13 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
-import os
+import platform
 from typing import Tuple
 
 
 def get_kernel_release() -> Tuple[int, int]:
     """Return Linux kernel version as (major, minor) tuple."""
-    major_str, minor_str = os.uname().release.split(".", maxsplit=2)[:2]
+    major_str, minor_str = platform.uname().release.split(".", maxsplit=2)[:2]
     return int(major_str), int(minor_str)
 
 
