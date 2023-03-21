424549fbaa77ff9330df8d97981df1f522be887e
Roi Agai <roi.agai@granulate.io>
2023-03-21 16:26:51 +0200

    lint

--- granulate_utils/metrics/__init__.py
+++ granulate_utils/metrics/__init__.py
@@ -5,13 +5,12 @@
 # (C) Datadog, Inc. 2018-present. All rights reserved.
 # Licensed under a 3-clause BSD style license (see LICENSE.bsd3).
 #
-from typing import Any, Dict, Iterable
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any, Dict, Iterable, Tuple, Union
 from urllib.parse import urljoin
 
 import requests
-from dataclasses import dataclass
-from datetime import datetime
-from typing import Any, Dict, Tuple, Union
 
 
 @dataclass
@@ -72,7 +71,7 @@ def join_url_dir(url: str, *args: Any) -> str:
 
 
 def set_individual_metric(
-        collected_metrics: Dict[str, Dict[str, Any]], name: str, value: Any, labels: Dict[str, str]
+    collected_metrics: Dict[str, Dict[str, Any]], name: str, value: Any, labels: Dict[str, str]
 ) -> None:
     """
     Add a metric to collected_metrics with labels in {name, value, labels} format.
@@ -89,10 +88,10 @@ def set_individual_metric(
 
 
 def set_metrics_from_json(
-        collected_metrics: Dict[str, Dict[str, Any]],
-        labels: Dict[str, str],
-        metrics_json: Dict[Any, Any],
-        metrics: Dict[str, str],
+    collected_metrics: Dict[str, Dict[str, Any]],
+    labels: Dict[str, str],
+    metrics_json: Dict[Any, Any],
+    metrics: Dict[str, str],
 ) -> None:
     """
     Extract metrics values from JSON response and add to collected_metrics in {name, value, labels} format.
@@ -106,7 +105,7 @@ def set_metrics_from_json(
 
 
 def samples_from_json(
-        labels: Dict[str, str], response_json: Dict[Any, Any], metrics: Dict[str, str]
+    labels: Dict[str, str], response_json: Dict[Any, Any], metrics: Dict[str, str]
 ) -> Iterable[Sample]:
     """
     Parse the JSON response and set the metrics
