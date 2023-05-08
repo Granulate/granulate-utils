4b31d6486ec29ead15ba8dc3f641013fb443947d
Michel Heily <michel.heily@granulate.io>
2023-05-08 17:10:13 +0300

    Handle zero ts in docker again

--- granulate_utils/containers/docker.py
+++ granulate_utils/containers/docker.py
@@ -3,6 +3,7 @@
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
 
+from datetime import datetime
 from typing import List, Optional
 
 import docker
@@ -35,12 +36,19 @@ class DockerClient(ContainersClientInterface):
     def get_runtimes(self) -> List[str]:
         return ["docker"]
 
+    @staticmethod
+    def _parse_docker_ts(ts: str) -> Optional[datetime]:
+        assert ts.endswith("Z")  # assert UTC
+        if ts.startswith("0001"):
+            return None
+        return isoparse(ts)
+
     @classmethod
     def _create_container(cls, container: docker.models.containers.Container) -> Container:
         pid: Optional[int] = container.attrs["State"].get("Pid")
-        created = isoparse(container.attrs["Created"])
+        created = cls._parse_docker_ts(container.attrs["Created"])
         assert created is not None
-        started_at = isoparse(container.attrs["State"]["StartedAt"])
+        started_at = cls._parse_docker_ts(container.attrs["State"]["StartedAt"])
         if pid == 0:  # Docker returns 0 for dead containers
             pid = None
         time_info = TimeInfo(create_time=created, start_time=started_at)
