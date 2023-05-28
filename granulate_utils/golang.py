1a0934dc453f68a739b90528664fea6ea2ef299e
pfilipko1 <piotr.filipkowski@intel.com>
2023-05-28 15:48:38 +0200

    cache based on start time

--- granulate_utils/golang.py
+++ granulate_utils/golang.py
@@ -7,7 +7,7 @@ import functools
 import struct
 from typing import Optional
 
-from psutil import NoSuchProcess, Process
+from psutil import NoSuchProcess, Process, pids
 
 from granulate_utils.linux.elf import read_elf_symbol, read_elf_va
 from granulate_utils.linux.ns import get_mnt_ns_ancestor
@@ -15,12 +15,22 @@ from granulate_utils.linux.process import is_kernel_thread, process_exe
 
 
 def is_golang_process(process: Process) -> bool:
-    return not is_kernel_thread(process) and get_process_golang_version(process) is not None
+    return not is_kernel_thread(process) and get_process_golang_version(process.create_time()) is not None
 
 
 @functools.lru_cache(maxsize=4096)
-def get_process_golang_version(process: Process) -> Optional[str]:
-    exe = process_exe(process)
+def get_process_golang_version(process_start_time: float) -> Optional[str]:
+    process = None
+    for pid in pids():
+        if Process(pid).create_time() == process_start_time:
+            process = Process(pid)
+
+    if process is None:
+        raise NoSuchProcess(process_start_time)
+    try:
+        exe = process_exe(process)
+    except:
+        return None
     elf_path = f"/proc/{get_mnt_ns_ancestor(process).pid}/root{exe}"
     try:
         symbol_data = read_elf_symbol(elf_path, "runtime.buildVersion", 16)
