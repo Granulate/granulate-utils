e809aa7ecaf5e81d98861152b3912b59d4c7d64a
slicklash <slicklash@gmail.com>
2023-07-21 13:47:40 +0300

    remove models

--- granulate_utils/config_feeder/core/models/autoscaling.py
+++ granulate_utils/config_feeder/core/models/autoscaling.py
@@ -1,10 +1,7 @@
-from datetime import datetime
 from enum import Enum
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 from pydantic import BaseModel
-
-from granulate_utils.config_feeder.core.models.collection import CollectorType
 
 
 class AutoScalingMode(str, Enum):
@@ -16,30 +13,3 @@ class AutoScalingMode(str, Enum):
 class AutoScalingConfig(BaseModel):
     mode: AutoScalingMode
     config: Dict[str, Any]
-
-
-class ClusterAutoScalingConfig(BaseModel):
-    autoscaling_config_id: str
-    cluster_id: str
-    mode: AutoScalingMode
-    config_hash: str
-    config_json: Dict[str, Any]
-    ts: datetime
-
-
-class ClusterAutoScalingConfigCreate(BaseModel):
-    collector_type: CollectorType
-    mode: AutoScalingMode
-    config_json: str
-
-
-class CreateClusterAutoScalingConfigRequest(BaseModel):
-    autoscaling_config: ClusterAutoScalingConfigCreate
-
-
-class CreateClusterAutoScalingConfigResponse(BaseModel):
-    autoscaling_config: ClusterAutoScalingConfig
-
-
-class GetClusterAutoScalingConfigsResponse(BaseModel):
-    autoscaling_configs: List[ClusterAutoScalingConfig]
