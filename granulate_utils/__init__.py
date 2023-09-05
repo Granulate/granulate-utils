306b45231c18a973d1db260a4a71ee0b85948ea2
Ouri Lipner <ouri.lipner@granulate.io>
2023-09-05 21:44:24 +0300

    lint

--- granulate_utils/__init__.py
+++ granulate_utils/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("granulate-utils")
