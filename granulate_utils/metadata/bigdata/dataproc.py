e6532585b56961ea9adb1c87df0260697dee7d73
collerek <collerek@gmail.com>
2023-10-23 15:29:29 +0200

    Collect hadoop version (#196)
    * Add hadoop_version to metadata, deliver with Cluster and NodeInfo
    
    * Fix lint
    
    * Revert all changes except metadata
    
    * Revert unnecessary import
    
    * add log message on failure to get hadoop version
    
    * run isort
    
    * make logger required
    
    * make logger optional and check for presence

--- granulate_utils/metadata/bigdata/dataproc.py
+++ granulate_utils/metadata/bigdata/dataproc.py
@@ -1,6 +1,14 @@
-from typing import List, Optional
+import logging
+import subprocess
+from typing import TYPE_CHECKING, List, Optional, Union
 
 VERSION_KEY = "DATAPROC_IMAGE_VERSION="
+HADOOP_VERSION_CMD = "hadoop version"
+
+if TYPE_CHECKING:
+    _LoggerAdapter = logging.LoggerAdapter[logging.Logger]
+else:
+    _LoggerAdapter = logging.LoggerAdapter
 
 
 def _get_environment_info() -> Optional[List[str]]:
@@ -9,6 +17,33 @@ def _get_environment_info() -> Optional[List[str]]:
             return f.readlines()
     except FileNotFoundError:
         pass
+    return None
+
+
+def get_hadoop_version(logger: Optional[Union[logging.Logger, _LoggerAdapter]]) -> Optional[str]:
+    """
+    Get the running hadoop version.
+
+    Sample cmd output:
+    >> Hadoop 2.9.2
+    >> Subversion https://bigdataoss-internal.googlesource.com/third_party/apache/hadoop -r b3e52921b6aaf2c68af220021eab42975114f7cb # noqa 501
+    >> Compiled by bigtop on 2021-07-12T21:30Z
+    >> Compiled with protoc 2.5.0
+    >> From source with checksum 3ea0fd8f2b9af855fb9f66c2e3130e3
+    >> This command was run using /usr/lib/hadoop/hadoop-common-2.9.2.jar
+
+    Extract the version from the first line.
+    """
+    try:
+        version_output = (
+            subprocess.check_output([HADOOP_VERSION_CMD], shell=True, stderr=subprocess.STDOUT)
+            .splitlines()[0]
+            .decode("utf-8")
+        )
+        return version_output.split(" ")[1]
+    except (subprocess.CalledProcessError, IndexError):
+        if logger:
+            logger.error("Failed to get hadoop version", exc_info=True)
     return None
 
 
