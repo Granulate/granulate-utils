dc7dd83088337437574e302f7048959212ee1a98
slicklash <slicklash@gmail.com>
2023-12-04 17:37:01 +0200

    up

--- granulate_utils/metrics/yarn/yarn_web_service.py
+++ granulate_utils/metrics/yarn/yarn_web_service.py
@@ -12,6 +12,13 @@ class YarnWebService(ABC):
         self._conf_url = f"{address}/conf"
 
     def conf(self) -> List[Dict[str, Any]]:
+        """
+        Get running service configuration
+
+        most recent config is returned
+
+        supported version: 2.8.3+
+        """
         return json_request(self._conf_url, {}).get("properties") or []
 
     def request(self, path: str, return_path: str, return_type: Type[T], **kwargs) -> T:
