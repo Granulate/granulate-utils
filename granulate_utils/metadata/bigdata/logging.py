289535828e0ff8f845d000ac8eb3cb46711fa924
collerek <collerek@gmail.com>
2024-07-08 16:17:17 +0200

    revert getting hadoop version (#253)

--- granulate_utils/metadata/bigdata/logging.py
+++
@@ -1,9 +0,0 @@
-import logging
-from typing import TYPE_CHECKING, Union
-
-if TYPE_CHECKING:
-    _LoggerAdapter = logging.LoggerAdapter[logging.Logger]
-else:
-    _LoggerAdapter = logging.LoggerAdapter
-
-LoggerOrAdapter = Union[logging.Logger, _LoggerAdapter]
